29251bb4f4cc1b5b6fe326cd48e073f7064e46a1
dada <dada_math@outlook.com>
2023-06-21 04:07:31 +0900

    new file:   11654.c 	new file:   11720.c 	new file:   2743.c 	new file:   27866.c 	new file:   9086.c

---
+++ 11720.c
@@ -0,0 +1,35 @@
+/*
+문제
+N개의 숫자가 공백 없이 쓰여있다. 이 숫자를 모두 합해서 출력하는 프로그램을 작성하시오.
+
+입력
+첫째 줄에 숫자의 개수 N (1 ≤ N ≤ 100)이 주어진다. 둘째 줄에 숫자 N개가 공백없이 주어진다.
+
+출력
+입력으로 주어진 숫자 N개의 합을 출력한다.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define SIZE 100
+
+int main(void){
+    int N;
+    char str[SIZE];
+    int sum = 0;
+    int num;
+
+    scanf("%d", &N);
+    scanf("%s", str);
+
+    for(int i=0;i<N;i++){
+        num = str[i] - '0';
+        sum += num;
+    }
+
+    printf("%d", sum);
+    
+    return 0;
+}
