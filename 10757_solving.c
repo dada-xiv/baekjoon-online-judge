4b63e80d8102cc942403a3fa3367fdb088d36fdc
dada <dada_math@outlook.com>
2023-06-24 06:56:47 +0900

    new file:   10757(easy).py 	modified:   10757_solving.c

--- 10757_solving.c
+++ 10757_solving.c
@@ -3,7 +3,7 @@
 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
 
 입력
-첫째 줄에 A와 B가 주어진다. (0 < A,B < 1010000)
+첫째 줄에 A와 B가 주어진다. (0 < A,B < 10^10000)
 
 출력
 첫째 줄에 A+B를 출력한다.
@@ -28,16 +28,56 @@ int main(){
 */
 
 #include <stdio.h>
+#include <string.h>
 
-#define MAX 10000
+#define MAX 100002
+
+int getMax(int a, int b){
+    if(a > b) return a;
+    else return b;
+}
 
 int main(){
-    int A[MAX];
-    int B[MAX];
+    char A[MAX];
+    char B[MAX];
+
+    int lenA, lenB, len;
+
+    int a[MAX] = {0};
+    int b[MAX] = {0};
+    int r[MAX] = {0};
+
+    int tmp;
 
     scanf("%s %s", A, B);
 
-    printf("%s, %s", A, B);
+    lenA = strlen(A);
+    lenB = strlen(B);
+    len = getMax(lenA, lenB);
+    
+    //printf("max len : %d\n", len);
+
+    for(int i=lenA-1;i>=0;i--){
+        a[i] = A[lenA-1-i] - '0';
+    }
+    for(int i=lenB-1;i>=0;i--){
+        b[i] = B[lenB-1-i] - '0';
+    }
+
+    for(int i=0;i<len;i++){
+        tmp = a[i]+b[i];
+        if(tmp<10){
+            r[i] = r[i] + tmp;
+        }else{
+            r[i] = r[i] + tmp - 10;
+            r[i+1] = r[i+1] + 1;
+            if(i+1==len) len++;
+        }
+    }
+
+    //for(int i=len-1;i>=0;i--){printf("%d ",a[i]);}printf("\n");
+    //for(int i=len-1;i>=0;i--){printf("%d ",b[i]);}printf("\n");
+    for(int i=len-1;i>=0;i--) printf("%d",r[i]);
 
     return 0;
 }
