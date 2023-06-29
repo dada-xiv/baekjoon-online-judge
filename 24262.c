6120e05822b9bf5bd1a76fe70b9704df45b0e6dc
dada <dada_math@outlook.com>
2023-06-29 17:04:52 +0900

    new file:   24262.c

---
+++ 24262.c
@@ -0,0 +1,28 @@
+/*
+문제
+
+알고리즘은 다음과 같다.
+
+MenOfPassion(A[], n) {
+    i = ⌊n / 2⌋;
+    return A[i]; # 코드1
+}
+
+입력
+첫째 줄에 입력의 크기 n(1 ≤ n ≤ 500,000)이 주어진다.
+
+출력
+첫째 줄에 코드1 의 수행 횟수를 출력한다.
+
+둘째 줄에 코드1의 수행 횟수를 다항식으로 나타내었을 때, 최고차항의 차수를 출력한다. 단, 다항식으로 나타낼 수 없거나 최고차항의 차수가 3보다 크면 4를 출력한다.
+ */
+
+#include <stdio.h>
+
+int main(void){
+    // Constant-time operation
+    printf("%d\n", 1);
+
+    // The zeroth order
+    printf("%d", 0);
+}
