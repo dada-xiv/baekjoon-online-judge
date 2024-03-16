4b62d17207036d00335325c8bf42432c396ddeb3
dada <dada_math@outlook.com>
2024-03-16 20:58:31 +0900

    new file:   19637.py 	new file:   19637_TLE.py

---
+++ 19637_TLE.py
@@ -0,0 +1,34 @@
+'''
+문제
+캐릭터가 가진 전투력을 기준으로 칭호를 붙여주려고 한다. 예를 들어, 전투력 10,000 이하의 캐릭터는 WEAK, 10,000 초과 그리고 100,000 이하의 캐릭터는 NORMAL, 100,000 초과 그리고 1,000,000 이하의 캐릭터는 STRONG 칭호를 붙여준다고 하자. 이를 IF문으로 작성한다면 아래와 같이 구현할 수 있다.
+
+if power <= 10000
+ print WEAK
+else if power <= 100000
+ print NORMAL
+else if power <= 1000000
+ print STRONG
+
+캐릭터의 전투력에 맞는 칭호를 출력하는 프로그램을 작성하자.
+
+입력
+첫 번째 줄에는 칭호의 개수 N (1 ≤ N ≤ 105)과 칭호를 출력해야 하는 캐릭터들의 개수 M (1 ≤ M ≤ 105)이 빈칸을 사이에 두고 주어진다. (1 ≤ N, M ≤ 105)
+두 번째 줄부터 N개의 줄에 각 칭호의 이름을 나타내는 길이 1 이상, 11 이하의 영어 대문자로만 구성된 문자열과 해당 칭호의 전투력 상한값을 나타내는 109 이하의 음이 아닌 정수가 주어진다. 칭호는 전투력 상한값의 비내림차순으로 주어진다. 
+N + 2번째 줄부터 M개의 각 줄에는 캐릭터의 전투력을 나타내는 음이 아닌 정수가 주어진다. 해당하는 칭호가 없는 전투력은 입력으로 주어지지 않는다.
+
+출력
+M개의 줄에 걸쳐 캐릭터의 전투력에 맞는 칭호를 입력 순서대로 출력한다. 어떤 캐릭터의 전투력으로 출력할 수 있는 칭호가 여러 개인 경우 가장 먼저 입력된 칭호 하나만 출력한다.
+'''
+from sys import stdin
+input = stdin.readline
+n, m = map(int, input().split())
+deals = list()
+for _ in range(n):
+  cname, maxdeal = map(str, input().split())
+  deals.append((cname, int(maxdeal)))
+for _ in range(m):
+  val = int(input())
+  for cn, mx in deals:
+    if val <= mx:
+      print(cn)
+      break
